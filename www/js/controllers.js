34bf58d4e32889b5df14f33c30d1ccc2349783ff
ericbichara <eric.bichara@gmail.com>
2014-08-04 21:00:43 +0200

    added pin dragging

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,24 +6,50 @@ app
 
 
 })
-.controller('ResultCtrl', function($scope, $cordovaGeolocation){
+.controller('ResultCtrl', function($scope, $cordovaGeolocation, $log){
     $scope.map = {
         center: {
             latitude: 45,
             longitude: -73
         },
-        zoom: 8
+        zoom: 8,
+        marker: {},
+        options: {draggable:true},
+        idKey: 1,
+        events: {
+            dragend: function (marker, eventName, args) {
+                $log.log("New Coords " + marker.getPosition());
+                $scope.$apply(
+                    function(){
+                        $scope.extraInfo = "New Coords: " +  marker.getPosition();
+                    }
+                );
+
+            }
+        }
     };
 
-    $scope.position = "";
+    $scope.extraInfo = "Starting Info";
+
+    $scope.position = "Starting Geo Location";
+
     $cordovaGeolocation.getCurrentPosition().then(function(position) {
         $scope.map.center.latitude = position.coords.latitude;
         $scope.map.center.longitude = position.coords.longitude;
         $scope.map.zoom = 14;
+        $scope.position = position;
 
+        $scope.map.marker = {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+        };
 
     }, function(err) {
-        console.log(err);
-    });
+        $scope.position = err;
+    },{enableHighAccuracy: true});
+
+    $scope.markerClick = function(){
+        $scope.extraInfo = "You did it ";
+    }
 
 });
